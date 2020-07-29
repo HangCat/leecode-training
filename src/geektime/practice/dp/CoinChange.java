059ba462d4a238b0cae35e8680893bc82de7a13c
zhouyp <zaiyuanfang40@163.com>
2020-07-29 21:53:05 +0800

    CoinChange

---
+++ src/geektime/practice/dp/CoinChange.java
@@ -0,0 +1,69 @@
+package geektime.practice.dp;
+
+/**
+ * @author zhouyp
+ * @program leecode-training
+ * @description 322. 零钱兑换
+ * 给定不同面额的硬币 coins 和一个总金额 amount。
+ * 编写一个函数来计算可以凑成总金额所需的最少的硬币个数。
+ * 如果没有任何一种硬币组合能组成总金额，返回 -1。
+ * 示例 1:
+ * 输入: coins = [1, 2, 5], amount = 11
+ * 输出: 3
+ * 解释: 11 = 5 + 5 + 1
+ * 示例 2:
+ * 输入: coins = [2], amount = 3
+ * 输出: -1
+ * 说明:
+ * 你可以认为每种硬币的数量是无限的。
+ * @create 2020-07-29
+ */
+public class CoinChange {
+
+	public static void main(String[] args) {
+		int[] coins = {1, 2, 5};
+		int target = 11;
+		System.out.println("coinChange = " + coinChange(coins, target));
+
+
+//		Optional<TreeNode> userOpt = Optional.ofNullable(user);
+//		Optional<String> roleIdOpt = userOpt.map(TreeNode::getRoleId);
+
+	}
+
+	static int coinChange(int[] coins, int amount) {
+		if (amount < 1) return 0;
+		return coinChangeRecursion(coins, amount);
+//		return coinChangeMemo(coins, amount, new int[amount]);
+	}
+
+
+	private static int coinChangeRecursion(int[] coins, int amount) {
+		if (amount < 0) return -1;
+		if (amount == 0) return 0;
+		int min = Integer.MAX_VALUE;
+		for (int coin : coins) {
+			final int res =
+					coinChangeRecursion(coins, amount - coin);
+			if (res >= 0 && res < min)
+				min = 1 + res;
+			else min = res;
+		}
+		return min;
+	}
+
+	private static int coinChangeMemo(int[] coins, int amount, int[] memo) {
+		if (amount < 0) return -1;
+		if (amount == 0) return 0;
+		if (memo[amount - 1] > 0) return memo[amount - 1];
+
+		int min = Integer.MAX_VALUE;
+		for (int coin : coins) {
+			int res = coinChangeMemo(coins, amount - coin, memo);
+			if (res >= 0 && res < min)
+				min = 1 + res;
+		}
+		memo[amount - 1] = (min == Integer.MAX_VALUE) ? -1 : min;
+		return memo[amount - 1];
+	}
+}
--- src/geektime/practice/tree/MaxDepth.java
+++ src/geektime/practice/tree/MaxDepth.java
@@ -11,6 +11,13 @@ import java.util.LinkedList;
  */
 public class MaxDepth {
 
+	public static void main(String[] args) {
+		final TreeNode node = TreeNode.stringToTreeNode("[3,9,20,null,null,15,7]");
+		System.out.println(maxDepthRecursion(node));
+		System.out.println(maxDepthBFS(node));
+		System.out.println(maxDepthDFS(node));
+	}
+
 	static int maxDepthRecursion(TreeNode root) {
 		if (root == null) return 0;
 		int maxLeft = maxDepthRecursion(root.left);
@@ -18,7 +25,7 @@ public class MaxDepth {
 		return Math.max(maxLeft, maxRight) + 1;
 	}
 
-	static int maxDepthIterator(TreeNode root) {
+	static int maxDepthBFS(TreeNode root) {
 		if (root == null) return 0;
 		final Deque<TreeNode> queue = new LinkedList<>();
 		int maxDepth = 0;
@@ -32,6 +39,50 @@ public class MaxDepth {
 			maxDepth++;
 		}
 		return maxDepth;
+	}
+
+	static int maxDepthDFS(TreeNode root) {
+		int res = 0;
+		if (root == null) return res;
+		final Deque<TMP> stack = new LinkedList<>();
+		TMP curr = new TMP(root, 1);
+		while ((!stack.isEmpty()) || (curr != null)) {
+
+//			if (curr.)
+			if (curr.node != null) {
+				stack.offerFirst(curr);
+				curr = new TMP(curr.node.left, curr.depth + 1);
+				/*if (curr.node.left == null) {
+					curr = null;
+				} else {
+					curr = new TMP(curr.node.left, curr.depth + 1);
+				}*/
+			} else{
+				curr = stack.pollFirst();
+				if (curr.node == null || curr.node.right == null) {
+					curr = new TMP(null, curr.depth);
+				} else {
+					curr = new TMP(curr.node.right, curr.depth + 1);
+				}
+				/*if (curr.node.right == null) {
+					curr = null;
+				} else {
+					curr = new TMP(curr.node.right, curr.depth + 1);
+				}*/
+			}
+			res = Math.max(res, curr.depth);
+		}
+		return res;
+	}
+
+	private static class TMP {
+		TreeNode node;
+		int depth;
+
+		TMP(TreeNode node, int depth) {
+			this.node = node;
+			this.depth = depth;
+		}
 	}
 
 
